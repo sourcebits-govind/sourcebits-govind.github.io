b1e7cbd085796333e6a5d3c80cf37528d43f7320
sourcebits-govind <govind.chaurasiya@sourcebits.com>
2016-07-14 18:48:27 +0530

    Timer countdown animation

--- js/application.js
+++ js/application.js
@@ -22,6 +22,9 @@ angular
     .module('ceb')
     .controller('questionController', questionController);
 
+// TIMER DEMO
+
+
 function questionController($scope,$http, $log) {
 
     var vm = this;
@@ -47,12 +50,31 @@ function questionController($scope,$http, $log) {
       vm.test = response.data;
   });
 
-  // $scope.$watch(function() {
-  //           return vm.answer;
-  //       }, function(current, original) {
-  //           $log.info('vm.answer was %s', original);
-  //           $log.info('vm.answer is now %s', current);
-  //       });
+
+
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+
+     
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        vm.display = minutes + ":" + seconds;
+        vm.percent = (timer * 100)/ duration;
+        $scope.$apply();
+    // console.log(  vm.display);
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+  var fiveMinutes = 60 * 1;   
+    startTimer(fiveMinutes); 
+
 
 
 }
